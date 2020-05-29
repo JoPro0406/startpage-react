dd7c4085dd5410b93905c51280158722823456ac
Jonas Seifert <jonas.seifert04@gmail.com>
2020-05-29 21:34:47 +0200

    Updates

---
+++ precache-manifest.0fbf5fb1d0ccdd74e2edb11d69650e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fed70fa2d87122ea19f1978534a607",
+    "url": "/startpage-react/index.html"
+  },
+  {
+    "revision": "1b3dc02724843fc65d38",
+    "url": "/startpage-react/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "6bc808a0def5d1f66293",
+    "url": "/startpage-react/static/js/2.937c78b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/startpage-react/static/js/2.937c78b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3dc02724843fc65d38",
+    "url": "/startpage-react/static/js/main.ffc0c05f.chunk.js"
+  },
+  {
+    "revision": "d3bce43b962c6766267d",
+    "url": "/startpage-react/static/js/runtime-main.51aa0f97.js"
+  }
+]);
