9d90ba74d025718119ecbb38de50249ebea8149e
Jonas Seifert <jonas.seifert04@gmail.com>
2020-06-02 21:41:08 +0200

    Updates

---
+++ precache-manifest.2910ea6acd497ca2c6592ecd99a8a68b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e419e36e80a3663b16ddcdf92ff12620",
+    "url": "/startpage-react/index.html"
+  },
+  {
+    "revision": "0b44cad139bcda292e54",
+    "url": "/startpage-react/static/css/main.d6ca1cde.chunk.css"
+  },
+  {
+    "revision": "da43a4bd36407585b02c",
+    "url": "/startpage-react/static/js/2.1c142d1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/startpage-react/static/js/2.1c142d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b44cad139bcda292e54",
+    "url": "/startpage-react/static/js/main.45eccab6.chunk.js"
+  },
+  {
+    "revision": "d3bce43b962c6766267d",
+    "url": "/startpage-react/static/js/runtime-main.51aa0f97.js"
+  },
+  {
+    "revision": "237c9b97731b5c13a26322831f178936",
+    "url": "/startpage-react/static/media/bg.237c9b97.jpg"
+  },
+  {
+    "revision": "57db916687187d4ebd1250f3e50db3a0",
+    "url": "/startpage-react/static/media/cotta.57db9166.png"
+  },
+  {
+    "revision": "5c138bd8243f4071344b86f0c61e0989",
+    "url": "/startpage-react/static/media/crunchyroll.5c138bd8.png"
+  },
+  {
+    "revision": "598d07e7761f5bca3a3f78e95b6db422",
+    "url": "/startpage-react/static/media/gmail.598d07e7.png"
+  },
+  {
+    "revision": "7a140126f807da7aef523177783722e6",
+    "url": "/startpage-react/static/media/reddit.7a140126.png"
+  },
+  {
+    "revision": "2a59b9849ab5b9c88c4a051ff477f481",
+    "url": "/startpage-react/static/media/search.2a59b984.png"
+  }
+]);
